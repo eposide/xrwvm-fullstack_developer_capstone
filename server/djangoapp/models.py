491eacf9d2861696e34035e1201ed0cea6d57bc7
frikbriers <frikbriers@gmail.com>
2024-10-25 13:33:29 +0200

    car make and model

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,9 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 
 # Create your models here.
@@ -12,6 +13,15 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(null=False, max_length=100)
+    website = models.URLField(null=True)
+   
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description + \
+               "Website: " + str(self.website)
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +33,23 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+
+    CAR_TYPE_CHOICES = {
+        "SEDAN": "Sedan",
+        "SUV": "SUV",
+        "WAGON": "Wagon",
+        "TRUCK": "Truck",
+    } 
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    type = models.CharField(null=False, max_length=30, choices=CAR_TYPE_CHOICES, default="Sedan")
+    year = models.IntegerField(null=False, validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+   
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Type: " + self.type + "," + \
+               "Year: " + str(self.year) + "," + \
+               "Car Make: " + str(self.car_make.name)
+    
